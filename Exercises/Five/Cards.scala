aa1636cd57c7bfbf0e8c1dd73344a9ff2f6aef3d
Rofudox <Rofudox@gmail.com>
2024-05-21 19:40:22 +0200

    Work on a card game

--- Exercises/Five/Cards.scala
+++ Exercises/Five/Cards.scala
@@ -6,20 +6,22 @@ case object Diamonds extends Suit
 case object Clubs extends Suit
 case object Spades extends Suit
 
-sealed trait Rank
-case object Two extends Rank
-case object Three extends Rank
-case object Four extends Rank
-case object Five extends Rank
-case object Six extends Rank
-case object Seven extends Rank
-case object Eight extends Rank
-case object Nine extends Rank
-case object Ten extends Rank
-case object Jack extends Rank
-case object Queen extends Rank
-case object King extends Rank
-case object Ace extends Rank
+sealed trait Rank {
+  def value: Int
+}
+case object Two extends Rank { val value = 2}
+case object Three extends Rank { val value = 3}
+case object Four extends Rank {val value = 4}
+case object Five extends Rank {val value = 5}
+case object Six extends Rank {val value = 6}
+case object Seven extends Rank {val value = 7}
+case object Eight extends Rank {val value = 8}
+case object Nine extends Rank {val value = 9}
+case object Ten extends Rank {val value = 10}
+case object Jack extends Rank {val value = 11}
+case object Queen extends Rank {val value = 12}
+case object King extends Rank {val value = 13}
+case object Ace extends Rank {val value = 14}
 
 case class Card(rank: Rank, suit: Suit)
 
@@ -40,5 +42,9 @@ def createDeck(): List[Card] = {
     val card = deck(randomCard)
     val curr_deck = deck.patch(randomCard, Nil, 1)
     (card, curr_deck)
+  }
+
+  def compareCard(card1: Card, card2: Card): Int = {
+    card1.rank.value.compareTo(card2.rank.value)
   }
 }
--- Exercises/Five/Fifth.scala
+++ Exercises/Five/Fifth.scala
@@ -1,20 +1,48 @@
 
-
+import scala.util.Random
 import Cards._
 
 object Fifth {
+  private var deck : List[Card] = Cards.createDeck()
+
   def main(args: Array[String]): Unit = {
-    var deck = Cards.createDeck()
-    var (newcard, current_deck) = Cards.newCard(deck)
-    deck = current_deck
-    println(s"Random card: ${newcard.rank} of ${newcard.suit}")
-    println(deck.length)
-    while(!deck.isEmpty) {
-      newCard(deck)}
-      println(s"Random card: ${newcard.rank} of ${newcard.suit}")
+    deck = Random.shuffle(deck)
+    var player1 : List[Card] = List.empty
+    var player2 : List[Card] = List.empty
+
+    while (deck.length > 2) {
+      val (card1, deck_1) = Cards.newCard(deck)
+      deck = deck_1
+      val (card2, deck_2) = Cards.newCard(deck)
+      deck = deck_2
+      val comparison = Cards.compareCard(card1, card2)
+      if (comparison > 0) {
+        println(s"${card1.rank} of ${card1.suit} is greater than ${card2.rank} of ${card2.suit}")
+        player1 = card1 :: player1
+        player1 = card2 :: player1
+      } else if (comparison < 0) {
+        println(s"${card1.rank} of ${card1.suit} is less than ${card2.rank} of ${card2.suit}")
+        player2 = card1 :: player2
+        player2 = card2 :: player2
+      } else {
+        println(s"${card1.rank} of ${card1.suit} is equal to ${card2.rank} of ${card2.suit}")
+      }
+      println(s"Remaining deck size: ${deck.length}")
+      println(player1.length)
+      println(player2.length)
+    }
   }
 
   def printCards(card: Card) : Unit = {
     println(s"${card.suit} of ${card.rank}")
+  }
+
+  def getNextCard(): Card = {
+    if (deck.isEmpty) {
+      throw new NoSuchElementException("No more Cards")
+    }
+    val card = deck.head
+    deck = deck.tail
+    card
   }
 }
